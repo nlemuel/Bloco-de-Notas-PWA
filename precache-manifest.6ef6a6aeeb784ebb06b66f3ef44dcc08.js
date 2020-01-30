fb377d4ba7c1f46db05a4432ebc953884b68646b
nlemuel <lemuelayres@gmail.com>
2020-01-30 10:18:14 -0300

    subindo gh-pages

---
+++ precache-manifest.6ef6a6aeeb784ebb06b66f3ef44dcc08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88bd043f917a84699a8",
+    "url": "/css/chunk-vendors.1ef5556e.css"
+  },
+  {
+    "revision": "894f1f340bc99ee52b98330a439cfc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8a6d3b687816ad08ff",
+    "url": "/js/app.4a9e354b.js"
+  },
+  {
+    "revision": "e88bd043f917a84699a8",
+    "url": "/js/chunk-vendors.0b170c96.js"
+  },
+  {
+    "revision": "36adaea0d60eab8555dacd423e222b29",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
